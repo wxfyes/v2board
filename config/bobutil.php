ece6d7bf74d0da0f9bb5488f4ee1775d85c5409d
tianquege <wangn5635@gmail.com>
2025-12-25 12:55:13 +0800

    config: 配置邮件营销独立邮箱 tianquee@gmail.com

--- config/bobutil.php
+++ config/bobutil.php
@@ -4,12 +4,12 @@ return [
     // 邮件营销专用邮箱配置（独立于 V2Board 系统邮箱）
     // 建议使用单独的 Gmail 邮箱，避免影响系统邮件额度
     'mail' => [
-        'host' => '',    // smtp.gmail.com
-        'port' => '',    // 587
-        'username' => '',    // your_marketing@gmail.com
-        'password' => '',    // Gmail 应用专用密码
-        'encryption' => '',    // tls
-        'from_address' => ''     // your_marketing@gmail.com
+        'host' => 'smtp.gmail.com',    // smtp.gmail.com
+        'port' => '587',    // 587
+        'username' => 'tianquee@gmail.com',    // your_marketing@gmail.com
+        'password' => 'nluu zrwy ivvx gtlx',    // Gmail 应用专用密码
+        'encryption' => 'tls',    // tls
+        'from_address' => 'tianquee@gmail.com'     // your_marketing@gmail.com
     ],
 
     // 用户注册xxx分钟没订单发送邮件
