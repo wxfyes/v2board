08a51b98777908e8a2bd59923dd9c427a49995c4
wxfyes <wangn5635@gmail.com>
2026-06-21 02:33:36 +0800

    revert: remove incorrect flow rewrite that broke MOMclash private protocol

--- app/Services/ServerService.php
+++ app/Services/ServerService.php
@@ -46,15 +46,7 @@ class ServerService
                     $server[$key]['encryption_settings'] = array_diff_key($server[$key]['encryption_settings'], array('private_key' => ''));
                 }
             }
-            $serverArray = $server[$key]->toArray();
-            if (isset($serverArray['flow'])) {
-                if ($serverArray['flow'] === 'mom-vision') {
-                    $serverArray['flow'] = 'xtls-rprx-vision';
-                } elseif ($serverArray['flow'] === 'mom-private') {
-                    $serverArray['flow'] = null;
-                }
-            }
-            $servers[] = $serverArray;
+            $servers[] = $server[$key]->toArray();
         }
 
 
